f25d21ae231ca24646d03c3f7ea3a316c659585b
dkyou7 <dkyou7@naver.com>
2020-08-19 15:39:30 +0900

    Initial commit

---
+++ src/main/java/com/ktnet/recruit/web/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.ktnet.recruit.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/policy")
+    public String dispPolicy(){
+        return "policy";
+    }
+
+    @GetMapping("/wrt01")
+    public String dispWrt01(){
+        return "wrt_01_";
+    }
+
+    @GetMapping("/wrt02")
+    public String dispWrt02(){
+        return "wrt_02_";
+    }
+}
